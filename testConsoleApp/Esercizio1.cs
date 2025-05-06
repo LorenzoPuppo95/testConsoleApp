807ce6cef65cade71972fd74df7496e7d539b014
LorenzoPuppo95 <puppolorenzo@live.it>
2025-05-06 10:45:51 +0200

    commit

---
+++ testConsoleApp/Esercizio1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testConsoleApp
+{
+    internal class Esercizio1
+    {
+        public void FizzBuzz()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 7 == 0 && IsPerfectSquare(i))
+                {
+                    Console.WriteLine("Paperone");
+                }
+                else if (i % 7 == 0)
+                {
+                    Console.WriteLine("Pippo");
+                }
+                else if (IsPerfectSquare(i))
+                {
+                    Console.WriteLine("Pluto");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        public static bool IsPerfectSquare(int number)
+        {
+            if (number < 0)
+            {
+                return false;
+            }
+
+            double sqrt = Math.Floor(Math.Sqrt(number));
+            return sqrt * sqrt == number;
+        }
+
+    }
+}
---
+++ testConsoleApp/EsercizioTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testConsoleApp
+{
+    internal class EsercizioTest
+    {
+        public static void WriteStiCazzi()
+        {
+            Console.WriteLine("Sti cazzi");
+        }
+        public void WriteStoCazzo()
+        {
+            Console.WriteLine("Sto cazzo");
+        }
+    }
+}
